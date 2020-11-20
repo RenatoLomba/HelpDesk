01b53c74c9e9da558d422f83e26aec38484b9f39
RenatoLomba <63821245+RenatoLomba@users.noreply.github.com>
2020-11-20 18:37:21 -0300

    validação de usuario

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+
+
 <html>
   <head>
     <meta charset="utf-8" />
@@ -12,6 +14,7 @@
         margin: 0 auto;
       }
     </style>
+
   </head>
 
   <body>
@@ -38,11 +41,34 @@
               <!-- Ínicio Formulário Login -->
               <form action="validar_login.php" method="post">
                 <div class="form-group">
-                  <input type="email" name="email" class="form-control" placeholder="E-mail">
+                  <input type="email" id="email" name="email" class="form-control" placeholder="E-mail">
                 </div>
                 <div class="form-group">
-                  <input type="password" name="senha" class="form-control" placeholder="Senha">
+                  <input type="password" id="senha" name="senha" class="form-control" placeholder="Senha">
                 </div>
+
+                <!-- //verifica se o GET possui o parametro 'login' -->
+                <?php if(isset($_GET['login'])) { ?>
+                  <?php if($_GET['login'] == 'erro_senha') { ?>
+                    <script>
+                      document.getElementById("senha").classList.add("border-danger");
+                      document.getElementById("email").value = '<?= $_GET['email'] ?>';
+                    </script>
+                    <div class="text-danger">
+                      Senha inválida
+                    </div>
+                  <?php } else if($_GET['login'] == 'erro_email') { ?>
+
+                    <script>
+                      document.getElementById("email").classList.add("border-danger");
+                      document.getElementById("senha").classList.add("border-danger");
+                    </script>
+                    <div class="text-danger">
+                      Usuário ou senha inválido(s)
+                    </div>
+                  <?php } ?>
+                <?php } ?>
+
                 <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
               </form>
               <!-- Fim Formulário Login -->
@@ -53,6 +79,8 @@
         </div>
       </div>
     </div>
+
+    <script src="script.js"></script>
 
   </body>
 
--- validar_login.php
+++ validar_login.php
@@ -1,13 +1,33 @@
 <?php
 
-    //metodo GET
-    /*echo '<pre>';
-    print_r($_GET);
-    echo '</pre>';*/
+$usuario_existe = false;
+$senha_correta = false;
 
-    //metodo POST
-    echo '<pre>';
-    print_r($_POST);
-    echo '</pre>';
+//usuários do sistema
+$usuarios_app = [
+    ['email' => 'adm@teste.com.br', 'senha' => 'root'],
+    ['email' => 'user@teste.com.br', 'senha' => '1234'],
+];
+
+//validação do usuário
+foreach($usuarios_app as $key => $value) {
+
+    if($_POST['email'] == $value['email']) {
+        $usuario_existe = true;
+        if($_POST['senha'] == $value['senha']) {
+            $senha_correta = true;
+        }
+    }
+
+}
+
+//autenticação
+if($usuario_existe && $senha_correta) {
+    echo 'Usuário autenticado';
+} else if($usuario_existe && ($senha_correta == false)) {
+    header('Location: index.php?login=erro_senha&email=' . ($_POST['email']));
+} else {
+    header('Location: index.php?login=erro_email');
+}
 
 ?>
